288a84e263e53c829c8b82ed54fe75e5658ecd87
„christophbuehler“ <christoph@cbuehler.li>
2023-08-30 17:30:13 +0200

    add cache for resolve ref

--- src/parser/resolve-refs.ts
+++ src/parser/resolve-refs.ts
@@ -12,6 +12,16 @@ import { REF_KEY } from "../constants";
 import { JSONSchema7 } from "json-schema";
 
 export const resolveRefs = (schema: JSONSchema7): JSONSchema7 => {
+  const cache = (() => {
+    const cacheMap = new Map<string, JSONSchema7>();
+    return (path: string) => {
+      if (cacheMap.has(path)) return cacheMap.get(path);
+      const res = get(schema, path);
+      cacheMap.set(path, res);
+      return res;
+    };
+  })();
+
   const resolve = <T extends JSONSchema7Value>(item: T): T =>
     !isObject(item)
       ? item
@@ -20,7 +30,7 @@ export const resolveRefs = (schema: JSONSchema7): JSONSchema7 => {
       : (mapValues(
           assign(
             omit(item, REF_KEY),
-            isString(item[REF_KEY]) ? get(schema, refToPath(item[REF_KEY])) : {}
+            isString(item[REF_KEY]) ? cache(refToPath(item[REF_KEY])) : {}
           ),
           resolve
         ) as T);
