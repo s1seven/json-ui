a8bc130cae23485998fd348c084487b3f4c4c368
„christophbuehler“ <christoph@cbuehler.li>
2023-08-30 12:59:54 +0200

    max h

--- src/json-ui-element.ts
+++ src/json-ui-element.ts
@@ -262,7 +262,7 @@ export class JsonUiElement extends LitElement {
                 class="ml-4 border border-slate-400 rounded-sm p-4 box-border text-xs text-slate-800"
               >
                 <pre
-                  class="max-w-[420px] overflow-auto"
+                  class="max-h-[420px] overflow-auto"
                 ><code class="break-all whitespace-pre-wrap">${unsafeHTML(
                   highlightPath(this.value, this.path)
                 )}</code></pre>
