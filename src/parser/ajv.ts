8c6c7f45fb77f640204a13caf3d70b201c54dfda
eamon0989 <eamon0989@gmail.com>
2024-09-09 11:25:34 +0200

    chore: update ajv to support 2019 draft

--- src/parser/ajv.ts
+++ src/parser/ajv.ts
@@ -1,20 +1,23 @@
-import Ajv from "ajv";
+import Ajv2019 from "ajv/dist/2019";
+import draft7MetaSchema from "ajv/dist/refs/json-schema-draft-07.json";
 import { AJV_ALLOWED_KEYWORDS } from "../constants";
 import addFormats from "ajv-formats";
 
-export const ajv = new Ajv();
+export const ajv = new Ajv2019();
 
 export const ajvFactory = () => {
-  const ajv = new Ajv();
-  addFormats(ajv);
-  AJV_ALLOWED_KEYWORDS.forEach((keyword) => {
-    ajv.addKeyword(keyword);
-  });
-  return ajv;
-}
+	const ajv = new Ajv2019();
+	ajv.addMetaSchema(draft7MetaSchema);
+	addFormats(ajv);
+	AJV_ALLOWED_KEYWORDS.forEach((keyword) => {
+		ajv.addKeyword(keyword);
+	});
+	return ajv;
+};
 
 addFormats(ajv);
+ajv.addMetaSchema(draft7MetaSchema);
 
 AJV_ALLOWED_KEYWORDS.forEach((keyword) => {
-  ajv.addKeyword(keyword);
+	ajv.addKeyword(keyword);
 });
